b6e158b945332237c81c508016d192edcbce9d03
Yichun Wang <wangyichunhome@pku.edu.cn>
2021-08-24 21:57:21 +0800

    华为机试

---
+++ HJ1.cpp
@@ -0,0 +1,32 @@
+﻿//HJ1 字符串最后一个单词的长度
+//描述
+//计算字符串最后一个单词的长度，单词以空格隔开，字符串长度小于5000。
+//
+//输入描述：
+//输入一行，代表要计算的字符串，非空，长度小于5000。
+//
+//输出描述：
+//输出一个整数，表示输入字符串最后一个单词的长度。
+// 
+//示例1
+//输入：
+//hello nowcoder
+//输出：
+//8
+//说明：
+//最后一个单词为nowcoder，长度为8
+
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    char str[5001];
+    cin.getline(str, 5000);
+    int len = 0;
+    for (int i = strlen(str) - 1; str[i] != ' ' && i >= 0; i--)
+        len++;
+    cout << len << endl;
+}
---
+++ HJ2.cpp
@@ -0,0 +1,37 @@
+﻿//HJ2 计算某字母出现次数
+//描述
+//写出一个程序，接受一个由字母、数字和空格组成的字符串，和一个字母，然后输出输入字符串中该字母的出现次数。不区分大小写，字符串长度小于500。
+//
+//输入描述：
+//第一行输入一个由字母和数字以及空格组成的字符串，第二行输入一个字母。
+//
+//输出描述：
+//输出输入字符串中含有该字符的个数。
+//
+//示例1
+//输入：
+//ABCabc
+//A
+//
+//输出：
+//2
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char str[10000];
+    char ch;
+    cin.getline(str, 10000);
+    cin >> ch;
+    int cnt = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ch || str[i] == ch - 32 || str[i] == ch + 32)
+            cnt++;
+    }
+    cout << cnt << endl;
+}
